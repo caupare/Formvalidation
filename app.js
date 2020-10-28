263a00fe8cd74a863aa06646286398c6df24241c
caupare <67455200+caupare@users.noreply.github.com>
2020-10-28 13:25:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function validate()
+{
+var fname = document.getElementById("fname").value;
+var lname = document.getElementById("lname").value;
+if( !fname || !lname || fname.includes(" ") || fname.includes(" "))
+{
+alert("Enter the Name correctly");
+return;
+}	
+
+var age = document.getElementById("age").value;
+var ag=/^(0?[1-9]|[1-9][0-9]|[1][0-9][0-4])$/
+if(!ag.test(age))
+{
+alert("Age cannot be "+age+"!!\n Enter Numeric values only!!");
+return; 
+}
+
+var bgroup = document.getElementById("bgroup").value;
+var bg= /^[aboABO]{0,1}(ab){0,1}(AB){0,1}[\-\+]{1}$/
+if(!bg.test(bgroup) || !bgroup )
+{
+ alert("Enter the blood group correctly");
+ return;
+}
+
+var no = document.getElementById("no").value;
+var no1=/^[6-9](\d){9}$/
+if(!no || !no1.test(no))
+{
+alert("Phone no cannot be "+no+"!!\n Enter Correct Phone no!!");
+return; 
+}
+
+var email = document.getElementById("email").value;
+var em= /^([a-zA-Z][a-zA-Z0-9\-\_]{2,20}[a-zA-Z]?)@([a-z0-9]{2,10}\.){1,4}[a-zA-Z0-9]{2,10}$/
+if(!em.test(email))
+{
+ alert("Enter the email correctly");
+ return;
+}
+
+
+var address = document.getElementById("address").value;
+var add = /^[a-zA-Z0-9\/]{2}[a-zA-Z0-9\.\-\_\,\ \/]{7,50}$/
+if(!add.test(address))
+{
+ alert("Enter the Address correctly");
+ return;
+}
+
+          
+
+}
